5546439744d7a57513ec8b0fbd2a9b5409c95f67
Nick Abell <darkforceyoda@gmail.com>
2023-02-24 11:50:14 -0600

    set up Routes, commit before working with api key

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import './Index.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Navbar />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+      </Routes>
     </>
   );
 }
