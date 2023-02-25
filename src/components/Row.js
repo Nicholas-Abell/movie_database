c56f52e58c9be77b4ec9ecd21afdf5f681c29383
Nick Abell <darkforceyoda@gmail.com>
2023-02-25 11:17:12 -0600

    + movie comp, Row: slider functionality, like icons

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import Movie from './Movie';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import React, { useEffect, useState } from 'react';
 
 const Row = ({ title, url }) => {
@@ -10,32 +12,35 @@ const Row = ({ title, url }) => {
         })
     }, [url]);
 
-    console.log(movies);
+
+    const slideLeft = () => {
+        let slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const slideRight = () => {
+        let slider = document.getElementById('slider');
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
 
     return (
         <>
             <h2 className='font-bold md:text-xl p-4'>{title}</h2>
-            <div className='relative flex items-center'>
-                <div id={'slider'}>
+            <div className='relative flex items-center group'>
+                <FaChevronLeft onClick={slideLeft} size={40} className='rounded-full text-black absolute opacity-50 hover:opacity-100 bg-white cursor-pointer z-10 hidden group-hover:block left-0' />
+                <div id={'slider'} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {
                         movies.map((movie, id) => {
                             return (
-                                <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                                    <img
-                                        className='w-full h-auto block'
-                                        src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`} alt={movie?.title}
-                                    />
-                                    <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100'>
-                                        <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full'>{movie?.title}</p>
-                                    </div>
-                                </div>
+                                <Movie key={id} movie={movie} />
                             )
                         })
                     }
                 </div>
+                <FaChevronRight onClick={slideRight} size={40} className='rounded-full text-black absolute opacity-50 hover:opacity-100 bg-white cursor-pointer z-10 hidden group-hover:block right-0' />
             </div>
         </>
     )
 }
 
-export default Row
+export default Row;
