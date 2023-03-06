d06f0136f5fbdab63281c33ae852cd0a745c295b
Nick Abell <darkforceyoda@gmail.com>
2023-03-05 21:40:41 -0600

    Poviders Section in MovieInfo

--- src/pages/MovieInfo.js
+++ src/pages/MovieInfo.js
@@ -13,7 +13,8 @@ const MovieInfo = () => {
     const { selectedMovie } = SelectedMovie();
     const isSmallScreen = useContext(ScreenSizeContext);
     const [streaming, setStreaming] = useState([]);
-    const [backupStream, setBackupStream] = useState([]);
+    const [buyList, setBuyList] = useState([]);
+    const [rentList, setRentList] = useState([]);
 
     const url = `https://api.themoviedb.org/3/movie/${selectedMovie?.id}/watch/providers?api_key=${tmdbKey}`
 
@@ -28,17 +29,19 @@ const MovieInfo = () => {
     };
 
     useEffect(() => {
-        axios.request(options)
-            .then(function (response) {
-                console.log(response.data[0]);
-                setStreaming(response.data[0].options.stream)
+        axios.get(url)
+            .then((res) => {
+                console.log(res.data.results.US);
+                setStreaming(res.data.results.US.flatrate);
+                setRentList(res.data.results.US.rent);
+                setBuyList(res.data.results.US.buy);
             })
             .catch(function (error) {
                 console.error(error);
-                axios.get(url)
-                    .then((res) => {
-                        console.log(res.data.results.US.flatrate[0].provider_name);
-                        setBackupStream(res.data.results.US.flatrate);
+                axios.request(options)
+                    .then(function (response) {
+                        console.log(response.data[0]);
+                        setStreaming(response.data[0].options.stream)
                     })
                     .catch(function (error) {
                         console.log(error);
@@ -46,33 +49,9 @@ const MovieInfo = () => {
             });
     }, []);
 
-    useEffect(() => {
-        console.log('back up stream:' + backupStream);
-    }, [backupStream])
-
-    // title:"Back to the Future"
-    // year:"1985"
-    // country:"us"
-    //  options:stream:
-    //      0:
-    //      provider:"Peacock Premium"
-    //      option:"Stream"
-    //      pricing:"Subs HD "
-    //      providerUrl:"https://www.peacocktv.com/watch/asset/movies/adventure/back-to-the-future/2a2c1229-2a42-3a10-bd50-2596e64e96d1"
-    //      1:
-    //      provider:"DIRECTV"
-    //      option:"Stream"
-    //      pricing:"Subs HD "
-    //      providerUrl:"https://www.directv.com/movies/Back-to-the-Future-RTB4bU5Xb2I3Y3NpL3Rwb1JpMmJydz09"
-    //      2:
-    //      provider:"USA Network"
-    //      option:"Stream"
-    //      pricing:"Subs "
-    //      providerUrl:"https://www.usanetwork.com/back-to-the-future/video/back-to-the-future/4230005"
-
     return (
         <>
-            <div className='w-full h-screen overflow-hidden' style={{ WebkitOverflowScrolling: 'hidden' }}>
+            <div className='w-full h-screen'>
                 <div className='w-full h-full'>
                     <div className='absolute w-full h-full bg-gradient-to-b from-black'></div>
                     <img
@@ -95,52 +74,71 @@ const MovieInfo = () => {
                             <p className='text-gray-400 text-sm'>Released: {selectedMovie?.release_date}</p>
                             <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200:'>{selectedMovie?.overview}</p>
                         </div>
-                        <div className='mt-16 rounded-lg bg-black opacity-80'>
+                        <div className='mt-16 rounded-lg bg-black opacity-80 border'>
                             <h1 className='text-3xl md:text-5xl text-center border-b py-2 font-bold'>Where To Watch</h1>
-
-                            {streaming.length > 0 || backupStream.length > 0
+                            {streaming.length > 0
                                 ? <div className='w-full p-4 flex justify-center items-center gap-12'>
-                                    {streaming?.map((stream) => stream.provider).includes('Netflix')
-                                        || backupStream?.map((stream) => stream.provider_name).includes('Netflix')
+                                    {streaming?.map((stream) => stream.provider_name).includes('Netflix')
                                         ? <div className='cursor-pointer'><SiNetflix className='text-red-600' size={50} /> Netflix</div>
                                         : <div className='text-gray-500 opacity-80'><SiNetflix className='text-gray-500 opacity-80' size={50} /> Netflix</div>}
-                                    {streaming?.map((stream) => stream.provider).includes('Hulu')
-                                        || backupStream?.map((stream) => stream.provider_name).includes('Hulu')
+                                    {streaming?.map((stream) => stream.provider_name).includes('Hulu')
                                         ? <div><SiHulu className='text-green-500' size={100} /></div>
                                         : <div><SiHulu className='text-gray-500 opacity-80' size={100} /></div>}
                                     {streaming?.map((stream) => stream.provider).includes('Disney Plus')
-                                        || backupStream?.map((stream) => stream.provider_name).includes('Disney Plus')
                                         ? <div className='text-3xl text-blue-400'>Disney+</div>
                                         : <div className='text-3xl text-gray-500 opacity-80'>Disney+</div>}
                                 </div>
                                 : <div className='p-4'>...Could Not Find a Streaming Service for this Product</div>
                             }
-                            <div className='border border-red-400 flex flex-col'>
-                                <h1>Providers:</h1>
-                                {
-                                    //Chatgpt why does this return null
-                                    streaming?.length > 0 ?
-                                        (
-                                            streaming?.map((stream, key) => {
-                                                return (
-                                                    <div key={key}>
-                                                        <div>{stream?.provider} test</div>
-                                                    </div>
-                                                )
-                                            })
-                                        )
-                                        : backupStream && backupStream.length > 0 ?
-                                            (
-                                                backupStream.map((stream, key) => {
-                                                    return (
-                                                        <div key={key}>
-                                                            <div>{stream?.provider_name} test</div>
-                                                        </div>
-                                                    )
-                                                })
-                                            )
-                                            : null
-                                }
+                            <div className='flex flex-col p-2'>
+                                <h1 className='text-3xl md:text-5xl text-center font-bold border-b-2'>Providers</h1>
+                                <div className='flex justify-between px-4 '>
+                                    <div className='h-full flex flex-col'>
+                                        <h1 className='font-bold text-xl border-b-2'>Stream</h1>
+                                        <ul>
+                                            {
+                                                streaming
+                                                    ?
+                                                    streaming?.map((stream, key) => {
+                                                        return (
+                                                            <li key={key}>{stream.provider_name}</li>
+                                                        )
+                                                    })
+                                                    : <div>none</div>
+                                            }
+                                        </ul>
+                                    </div>
+                                    <div className='h-full flex flex-col'>
+                                        <h1 className='font-bold text-xl border-b-2'>Buy</h1>
+                                        <ul >
+                                            {
+                                                buyList
+                                                    ?
+                                                    buyList?.map((buy, key) => {
+                                                        return (
+                                                            <li key={key}>{buy.provider_name}</li>
+                                                        )
+                                                    })
+                                                    : <div>none</div>
+                                            }
+                                        </ul>
+                                    </div>
+                                    <div className='h-full flex flex-col'>
+                                        <h1 className='font-bold text-xl border-b-2'>Rent</h1>
+                                        <ul >
+                                            {
+                                                rentList
+                                                    ?
+                                                    rentList?.map((rent, key) => {
+                                                        return (
+                                                            <li key={key}>{rent.provider_name}</li>
+                                                        )
+                                                    })
+                                                    : <div>none</div>
+                                            }
+                                        </ul>
+                                    </div>
+                                </ div>
                             </div>
                         </div>
                     </div>
