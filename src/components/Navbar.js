15de9b7ae83cd9518c7c2852cfc45d54bcf50c27
Nick Abell <darkforceyoda@gmail.com>
2023-03-06 23:38:01 -0600

    Made Navbar Responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,29 +19,29 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-30 w-full absolute'>
       <Link to='/'>
-        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>Movie Database</h1>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer hover:text-red-800'>Movie Database</h1>
       </Link>
       <div className='flex justify-center items-center gap-4'>
         <Link to='/search'>
-          <AiOutlineSearch size={25} />
+          <AiOutlineSearch size={25} className='hover:text-gray-500' />
         </Link>
         {
           user?.email
             ? (
               <div>
                 <Link to='/account'>
-                  <button className='pr-4'>Account</button>
+                  <button className='pr-4 hover:text-gray-500'>Account</button>
                 </Link>
-                <button onClick={handleLogOut} className='bg-red-600 px-6 py-2 rounded cursor-pointer'>Log Out</button>
+                <button onClick={handleLogOut} className='bg-red-600 px-6 py-2 rounded cursor-pointer hover:bg-red-800'>Log Out</button>
               </div>
             )
             : (
               <div>
                 <Link to='/login'>
-                  <button className='pr-4'>Log In</button>
+                  <button className='pr-4 hover:text-gray-500'>Log In</button>
                 </Link>
                 <Link to='/signup'>
-                  <button className='bg-red-600 px-6 py-2 rounded cursor-pointer'>Sign Up</button>
+                  <button className='bg-red-600 px-6 py-2 rounded cursor-pointer hover:bg-red-800'>Sign Up</button>
                 </Link>
               </div>
             )
