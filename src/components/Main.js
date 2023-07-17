0708c6d029860ae86e7bc01452d48adde01de531
Nick <darkforceyoda@gmail.com>
2023-07-17 11:48:16 -0500

    loading skeleton for Hero

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,22 @@
-import axios from 'axios';
-import React, { useEffect, useState, useContext } from 'react';
-import { request } from '../Request';
-import { ScreenSizeContext } from '../context/ScreenSizeContext';
-import ButtonPalette from './ButtonPalette';
+import axios from "axios";
+import React, { useEffect, useState, useContext } from "react";
+import { request } from "../Request";
+import { ScreenSizeContext } from "../context/ScreenSizeContext";
+import ButtonPalette from "./ButtonPalette";
+import MovieSkeleton from "./skeleton/MovieSkeleton";
+import HeroSkeleton from "./skeleton/HeroSkeleton";
 
 const Main = () => {
   const isSmallScreen = useContext(ScreenSizeContext);
   const [movies, setMovies] = useState([]);
   const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    document.body.style.overflow = 'scroll';
+    document.body.style.overflow = "scroll";
     axios.get(request.requestPopular).then((res) => {
       setMovies(res.data.results);
+      setLoading(false);
       console.log(res.data.results);
     });
   }, []);
@@ -21,36 +25,48 @@ const Main = () => {
     if (movies.length > 0) {
       const random = Math.floor(Math.random() * movies.length);
       setMovie(movies[random]);
-      console.log('movie: ' + movie);
+      console.log("movie: " + movie);
     }
   }, [movies]);
 
   const truncateString = (str, num) => {
     if (str?.length > num) {
-      return str.slice(0, num) + '...'
+      return str.slice(0, num) + "...";
     } else {
       return str;
     }
-  }
+  };
 
   return (
-    <div className='w-full h-[550px]'>
-      <div className='w-full h-full'>
-        <div className='absolute w-full h-[550px] bg-gradient-to-r from-black'></div>
-        <img
-          className='w-full h-full object-cover'
-          src={!isSmallScreen ? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}` : `https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
-          alt={'movie_main'}
-        />
-        <div className='absolute w-full top-[20%] p-4 md:p-8'>
-          <h1 className='text-3xl md:text-5xl'>{movie?.title}</h1>
+    <div className="w-full h-[550px]">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-[550px] bg-gradient-to-r from-black"></div>
+        {loading ? (
+          <HeroSkeleton />
+        ) : (
+          <img
+            className="w-full h-full object-cover"
+            src={
+              !isSmallScreen
+                ? `https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`
+                : `https://image.tmdb.org/t/p/original/${movie?.poster_path}`
+            }
+            alt={"movie_main"}
+          />
+        )}
+        <div className="absolute w-full top-[20%] p-4 md:p-8">
+          <h1 className="text-3xl md:text-5xl">{movie?.title}</h1>
           <ButtonPalette movie={movie} setMovieBool={true} />
-          <p className='text-gray-400 text-sm'>Released: {movie?.release_date}</p>
-          <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200:'>{truncateString(movie?.overview, 250)}</p>
+          <p className="text-gray-400 text-sm">
+            Released: {movie?.release_date}
+          </p>
+          <p className="w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200:">
+            {truncateString(movie?.overview, 250)}
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Main;
--- src/components/skeleton/MovieSkeleton.js
+++ src/components/skeleton/MovieSkeleton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const MovieSkeleton = () => {
   return (
-    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] h-[90px] md:h-[160px] inline-block cursor-pointer relative p-2 bg-gray-200 animate-pulse rounded-md"></div>
+    <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] h-[90px] md:h-[160px] inline-block cursor-pointer relative p-2 bg-gray-400 animate-pulse rounded-md"></div>
   );
 };
 export default MovieSkeleton;
